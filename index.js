da6c0a6ee1e5e8aaa5feab3d9e52e057e676cb38
xavilopez <xavi_lopez011@hotmail.com>
2017-03-07 12:06:38 -0300

    test

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function onLoad() {
   vrView = new VRView.Player('#vrview', {
     width: '100%',
     height: 480,
-    video: './src/videotest.mp4',
+    video: './src/congo_2048.mp4',
     is_stereo: true,
     //is_debug: true,
     //default_heading: 90,
